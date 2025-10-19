83996a010c1633701d78aab360bdfdf8d6ed2786
Xavier Felixan Mijaata <xavier357159@gmail.com>
2025-10-19 20:15:29 +0800

    CAPITALIZATION

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ def prepare_dataset(tokenizer, split='train', num_samples=None):
 
     # Tokenize the text
     tokenized_inputs = tokenizer(
-        list(df['Title']),
+        list(df['title']),
         padding='max_length',
         truncation=True,
         max_length=128,  # Adjust if necessary
