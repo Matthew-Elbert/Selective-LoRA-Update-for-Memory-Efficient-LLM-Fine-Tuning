9b532b552f2ec87ae867afed8ca849497765a59a
Xavier <xavier357159@gmail.com>
2026-02-17 18:09:16 +0800

    Test training without seed

--- GPU_and_predict.py
+++ GPU_and_predict.py
@@ -17,11 +17,11 @@ import datetime
 import json
 import gc
 
-# torch.manual_seed(67)
-# np.random.seed(67)
-# random.seed(67)
-# if torch.cuda.is_available():
-#    torch.cuda.manual_seed_all(67)
+torch.manual_seed(67)
+np.random.seed(67)
+random.seed(67)
+if torch.cuda.is_available():
+   torch.cuda.manual_seed_all(67)
 
 # Global variables to track peak memory usage DURING TRAINING
 peak_gpu_memory_during_training = 0
@@ -271,7 +271,7 @@ results = {
     'trainable_parameters': sum(p.numel() for p in model.parameters() if p.requires_grad)
 }
 
-with open('./results_{timestamp}/training_results_detailed_test.json', 'w') as f:
+with open(f'./results_{timestamp}/training_results_detailed_test.json', 'w') as f:
     json.dump(results, f, indent=2)
 
 print(f"\nResults saved to 'training_results_detailed.json'")
